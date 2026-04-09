dd852d32c2a710dc4367b00c69834644127b3a58
MavihsLH <99912017+MavihsLH@users.noreply.github.com>
2026-04-09 11:52:02 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPalindrome(string &s) {
+        int leftPointer = 0;
+        int rightPointer = s.size() - 1;
+        bool isAlnum = false;
+
+        for(auto &c : s){
+            if(isalnum(c))
+            {
+               isAlnum = true; 
+            }
+            if(!isAlnum){
+                return true;
+            }
+        }
+
+        while(leftPointer < rightPointer){
+            while(!isalnum(s[leftPointer])){
+                leftPointer++;
+            }
+            while(!isalnum(s[rightPointer])){
+                rightPointer--;
+            }
+
+            if(std::tolower(s[leftPointer]) != std::tolower(s[rightPointer])){ 
+                return false;
+            }
+            leftPointer++;
+            rightPointer--;
+        }
+        return true;
+    }
+};
